fa10a7c904a2ce609210a64f54101b33c09984d3
GrupoPoo-2 <167116265+GrupoPoo-2@users.noreply.github.com>
2024-04-15 18:54:29 -0500

    Add files via upload

---
+++ src/carro/Carro.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package carro;
+
+/**
+ *
+ * @author migueldedios
+ */
+public class Carro {
+
+ 
+    private String marca;
+    private String modelo;
+    private int año;
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public int getAño() {
+        return año;
+    }
+
+    public void setAño(int año) {
+        this.año = año;
+    }
+}
+    
+
